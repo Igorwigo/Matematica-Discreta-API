a12491507a5469520840bd1ce5613ad2817025fa
Igor Valadares Queiroz <igor.v.q@rede.ulbra.br>
2020-02-28 21:40:11 -0300

    Aula dia 28/02/2020 uniao

--- matematica discreta.py
+++ matematica discreta.py
@@ -1,44 +1,54 @@
-class Conjuntos():
+class Conjunto():
     def __init__(self):
         self.lista = []
-        self.nome=""
         self.quant = 0
-
-    def nome(self,valor1):
-        self.nome=valor1
 
     def inserir(self,valor):
         if valor in self.lista:
-            return False
+            return print("Já possui!")
         else:
             self.lista.append(valor)
-            self.quant+=1
+            self.quant += 1
             return self.lista
-        
+            
     def imprimir(self):
-            print("Nome: ",self.nome,", Conjunto: ","{",*self.lista,"}.", sep="")
-         
+        print(*self.lista, sep=',')
+
     def tamanho(self):
         return self.quant
-    def pertence(self,valor):
+
+    def pertence(self, valor):
         return valor in self.lista
-                
 
+    def uniao(self,b):
+        uniao = []
+        #------------------------------------------------------
+        #Elemento neutro---------------------------------------
+        if self.lista == []:
+            for i in b.lista:
+                uniao.append(i)
+
+            return("A é vazio!",b.lista)
+        if b.lista == []:
+            for i in self.lista:
+                uniao.append(i)
+
+            return("B É vazio!",self.lista)
+        #------------------------------------------------------
+        #Idempotencia------------------------------------------
+        if self.lista == b.lista:
+            return self.lista
+        #------------------------------------------------------
+        #União-------------------------------------------------
+        for i in b.lista:
+            uniao.append(i)
+
+        for i in self.lista:
+            if i not in b.lista:    
+                uniao.append(i)
+
+        return uniao
     
-teste=Conjuntos()
 
-teste.inserir("a")
-print(teste.inserir("a"))
-teste.inserir("b")
-
-
-teste.imprimir()
-
-print(teste.pertence("abc"))
-
-
-
-
-
-
-
+            
+            
